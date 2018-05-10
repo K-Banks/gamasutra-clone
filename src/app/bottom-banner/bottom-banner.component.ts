c8b4a5e4d38741c7e0faaa1fd0545eec1815f8bd
Kayl Eubanks <kayleubanks@gmail.com>
2018-05-09 17:58:42 -0700

    Update bottom banner to utilize links lists in .ts file

--- src/app/bottom-banner/bottom-banner.component.ts
+++ src/app/bottom-banner/bottom-banner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Link } from './../model/link.model';
 
 @Component({
   selector: 'app-bottom-banner',
@@ -6,6 +7,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bottom-banner.component.css']
 })
 export class BottomBannerComponent implements OnInit {
+
+  techLinks1: Link[] = [
+    new Link('Black Hat', '#'),
+    new Link('Content Marketing Institute', '#'),
+    new Link('Content Marketing World', '#'),
+    new Link('Dark Reading', '#'),
+  ]
+
+  techLinks2: Link[] = [
+    new Link('Enterprise Connect', '#'),
+    new Link('GDC', '#'),
+    new Link('Gamasutra', '#'),
+    new Link('HDI', '#'),
+  ]
+
+  techLinks3: Link[] = [
+    new Link('ICMI', '#'),
+    new Link('InformationWeek', '#'),
+    new Link('INsecurity', '#'),
+    new Link('Interop ITX', '#'),
+  ]
+
+  techLinks4: Link[] = [
+    new Link('Network Computing', '#'),
+    new Link('No Jitter', '#'),
+    new Link('Service Management World', '#'),
+    new Link('XRDC', '#'),
+  ]
+
+  communityLinks: Link[] = [
+    new Link('Content Marketing', '#'),
+    new Link('Enterprise IT', '#'),
+    new Link('Enterprise Communications', '#'),
+    new Link('Game Development', '#'),
+    new Link('Information Security', '#'),
+    new Link('IT Services & Support', '#'),
+  ]
+
+  workingLinks: Link[] = [
+    new Link('Advertising Contracts', '#'),
+    new Link('Event Calendar', '#'),
+    new Link('Tech Marketing', '#'),
+    new Link('Solutions', '#'),
+    new Link('Contact Us', '#'),
+    new Link('Licensing', '#'),
+  ]
+
+  legalLinks: Link[] = [
+    new Link('Terms of Service', '#'),
+    new Link('Privacy Statement', '#'),
+    new Link('Legal Entities', '#'),
+  ]
 
   constructor() { }
 
