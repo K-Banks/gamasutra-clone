d34dbc1738daa3e6da29f348378fd97f9c573d4a
Kayl Eubanks <kayleubanks@gmail.com>
2018-05-11 09:33:29 -0700

    Attempt to utilize a component or model to auto-generate smaller content sections

--- src/app/left-bar/left-bar.component.ts
+++ src/app/left-bar/left-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ContentSideBar } from '../model/ContentSideBar.model';
 
 @Component({
   selector: 'app-left-bar',
@@ -11,5 +12,13 @@ export class LeftBarComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  let latestJobs: ContentSideBar = new ContentSideBar(
+    "Latest Jobs",
+    "https://twimgs.com/gamasutra/images/icon_jobs.png",
+    ["Stardock Systems, Inc", "Gear Inc.", "Sucker Punch Productions", "Sucker Punch Productions", "Skydance Interactive", "Skydance Interactive"],
+    ["Associate Art Producer", "[Vietnam] Game Designer", "Outsource Artist", "Senior Environment Artist", "Software Engineer", "Gameplay Engineer"],
+    ["#", "#", "#", "#", "#", "#"]
+  );
 
 }
